67d890c722f0560f4870a95e72c1c4fe53904d51
flpego <flaviopegoraro2@gmail.com>
2024-04-03 19:27:08 -0300

    form con reactiveForms finalizado

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,40 +1,47 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
-  styleUrl: './reactive-forms.component.scss'
+  styleUrl: './reactive-forms.component.scss',
 })
 export class ReactiveFormsComponent {
-
   signUpForm = this.formBuilder.group({
-    userName:this.formBuilder.control('', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]),
-    email: this.formBuilder.control("", [Validators.required ,Validators.email]),
-    password: this.formBuilder.control("", [Validators.required, Validators.minLength(8)]),
-    confirmPassword: this.formBuilder.control("", [Validators.required, Validators.minLength(8)])
+    userName: this.formBuilder.control('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9]*'),
+    ]),
+    email: this.formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
 
   get userNameControl() {
-    return this.signUpForm.get('userName')
+    return this.signUpForm.get('userName');
   }
-  
+
   get emailControl() {
-    return this.signUpForm.get('email')
+    return this.signUpForm.get('email');
   }
   get passwordControl() {
-    return this.signUpForm.get('password')
-  }
-  get confirmPasswordControl() {
-    return this.signUpForm.get('confirmPassword')
+    return this.signUpForm.get('password');
   }
 
+  constructor(private formBuilder: FormBuilder) {}
 
-
-  constructor(private formBuilder : FormBuilder) {
-  }
-
-  onSubmit (): void {
-    alert("Usuario registrado con exito")
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      alert("Usuario registrado con exito")
+      console.log(this.signUpForm);
+    } else {
+      alert("Completa los campos antes de continuar")
+      this.signUpForm.markAllAsTouched();
+    }
   }
 }
